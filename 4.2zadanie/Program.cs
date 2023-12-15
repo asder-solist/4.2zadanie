5bba749096ce2c99a145680d6daecb35bb56ed61
clown <artemkoka@bk.ru>
2023-12-15 16:27:05 +0300

    Добавьте файлы проекта.

---
+++ 4.2zadanie/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._2zadanie
+{
+    internal class Program
+    {
+        public static string[] SplitLine(string line)
+        {
+            string[] subs = line.Split(' ');
+            return subs;
+        }
+
+        public static void ReversWords(string words)
+        {
+            string[] subs = SplitLine(words);
+            for (int i = subs.Length -1; i >= 0; i--)
+            {
+                Console.Write(subs[i] + " ");
+            }
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение с пробелом после каждого слова");
+            string line = Console.ReadLine();
+
+            ReversWords(line);
+        }
+    }
+}
